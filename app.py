0d9088888233cede892b844907aa8e01b0f5b072
Ujjwal <ujjwal.nanda@stratlytics.com>
2023-05-24 18:00:26 +0530

    First Commit

--- app.py
+++ app.py
@@ -8,44 +8,50 @@ from pywebio import start_server
 import time
 from resume_scoring import predict_resume_scoring
 app = Flask(__name__)
+import os
+import shutil
 
 def cv_parser():
     
-  
-    put_markdown("Welcome to CV Parser Application..!")
+    
+    put_code("----- Welcome to CV Parser Application -----")
     count=0
     add_more = True
+    
     while add_more: 
         
         info = input_group("Enter Details : ",[
           input("Enter Job Title you are looking for :", name='job_title'),
           input("Enter Skillset you are looking for :", name='skill'),
           input("Enter Education you are looking for :", name='education'),
-          input("Enter years of experience you are looking for :", name='experience',type=NUMBER)
+          
+          input("Enter years of experience you are looking for :", name='experience',type=FLOAT)
         ])
-        
         print(info['job_title'], info['skill'],info['education'],info['experience'])
         job_title=info['job_title']
         skill=info['skill']
         education = info['education']
-        experience = float(info['experience'])
+        experience = info['experience']
         
             
         put_text('You have searched fors Job Tile : ',job_title)
         put_text('Selected Skillset :' ,skill)
+        put_text('Required Degree :' ,education)
+        put_text('Required experience :' ,experience)
         put_text('------------------------------------------------')
         #put_text(df)
         with put_loading():
-            put_text("We are getting the top matching CVs Plz Wait...")
+            put_code("Plz Wait..We are working on magic !! Fetching top CVs for you..")
             time.sleep(3)  # Some time-consuming operations
         put_text("Matched Results : ")
         
+        
         selected_resume = predict_resume_scoring(job_title,skill,education,experience)
         put_table([
-                    {"Job_Title":job_title,"SkillSet":skill,"TopCVFound":selected_resume,"Score":70}
-                ], header=["Job_Title", "SkillSet","TopCVFound","Score"]) 
+                    {"Job_Title":job_title,"SkillSet":skill,"TopCVFound":selected_resume}
+                ], header=["Job_Title", "SkillSet","TopCVFound"]) 
 
-           
+        put_code("**Note: All the matched CVs are stored in 04_Matched_CVs_pdf folder")   
         
         add_more = actions(label="Would you like to search more ?", 
                         buttons=[{'label': 'Yes', 'value': True}, 
@@ -58,13 +64,13 @@ def cv_parser():
         put_text("--------------------------")
         #clear(scope=- 1) 
       
-    put_text("Thank You for using the CV Parser Application ..! ")
+    put_code("----- Thank You for using the CV Parser Application -----")
 
 
 
-app.add_url_rule('/cv_parser', 'webio_view', webio_view(cv_parser,session_expire_seconds=120),
+app.add_url_rule('/cv_parser', 'webio_view', webio_view( cv_parser),
             methods=['GET', 'POST', 'OPTIONS'])
 
 
 
-app.run()
+app.run(host='localhost', port=80)
